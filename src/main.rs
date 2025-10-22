d2de394c4c86fb104392dba4f521b41e9176c674
BattleDex <143239271+BattleDemon@users.noreply.github.com>
2025-10-23 09:27:38 +1100

    brbt4h5

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use calamine::{open_workbook, Reader, Xlsx};
+use iced::{button, Align, Button, Column, Container, Element, Length, Row, Sandbox, Settings, Text};
 
 fn main() {
     println!("Hello, world!");
