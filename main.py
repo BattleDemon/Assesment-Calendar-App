70d5d97103f72d280f8ced025f7ecbd43f1a4ee0
BattleDex <143239271+BattleDemon@users.noreply.github.com>
2025-10-24 10:10:03 +1100

    tjyj65

---
+++ main.py
@@ -0,0 +1,46 @@
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+    QPushButton,
+    QListWidget,
+    QStackedWidget,
+    QCalendarWidget,
+    QLabel,
+    QSpinBox,
+    QTextEdit,
+    QInputDialog,
+    QComboBox,
+    QListWidgetItem,
+    QMessageBox,
+    QTimeEdit,
+    QLineEdit,
+    QTabWidget,
+    QDateEdit,
+    QTableWidget,
+    QTableWidgetItem,
+    QHeaderView
+) 
+from PyQt6.QtCore import QDate, Qt
+from PyQt6.QtGui import QTextCharFormat, QBrush, QColor, QFont
+from PyQt6.QtCore import QTime
+from datetime import datetime
+import json
+import os
+import sys
+
+class CalendarApp(QMainWindow):
+    def __init__(self):
+        super(CalendarApp, self).__init__()
+
+        self.show()
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = CalendarApp()
+    sys.exit(app.exec())
+
+
