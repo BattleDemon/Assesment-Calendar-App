6007e8f145fdc471a94f6aa08fc3a89457c9aa7c
BattleDex <143239271+BattleDemon@users.noreply.github.com>
2025-11-06 14:39:42 +1100

    fixed filter nows now working on filling out data in FixedCols so its not just the first with the value

--- extractdata.py
+++ extractdata.py
@@ -14,22 +14,20 @@ def _find_header_row(raw: pd.DataFrame, max_scan: int = 20) -> int:
         if first4 == ["week", "day", "date", "events"]:
             return i
         return 0
-'''
-def _stop_at_blank_week(df_in: pd.DataFrame) -> pd.DataFrame:
-    # Don't export rows after the first week = ""
-    keep_rows = []
-    
-    for _, r in df_in.iterrows():
-        week_val = str(r.get("Week", "")).strip().lower()
-        if week_val == "" or week_val == "nan":
-            break
-        keep_rows.append(r)
 
-    if not keep_rows:
-        return df_in.iloc[0:0].copy()
-    
-    return pd.DataFrame(keep_rows, columns=df_in.columns)
-    '''
+def fillOutFixedColdata(df: pd.DataFrame):
+
+    for col in FIXEDCOL:
+        for row in df[col]:
+            if row != None:
+                lastVal = row
+
+            else:
+                row = lastVal
+                
+
+    print(df)
+    return df
 
 def _require_columns(df: pd.DataFrame, cols: list[str], label: str ):
     missing = [c for c in cols if c not in df.columns]
@@ -40,20 +38,9 @@ def _checkForTempValue(df: pd.DataFrame, cols: list[str] ) -> pd.DataFrame:
 
     className = cols[0]
 
-    for _, r in df.iterrows():
-
-        keep_rows = []
-
-        task_name = str(r.get(className, "")).strip()
-        if task_name != "Select Class":
-            keep_rows.append(r)
-
-        print(keep_rows)
+    keep_rows = df[df[className] != "Select Class"]
     
     return pd.DataFrame(keep_rows)
-
-            
-            
 
 
 def extract_sheet1_json(xlsx_path: str, outdir: str = "."):
@@ -65,21 +52,24 @@ def extract_sheet1_json(xlsx_path: str, outdir: str = "."):
     _require_columns(df, Y11, "Year 11")
     _require_columns(df, Y12, "Year 12")
 
+    df = fillOutFixedColdata(df)
+
     #data = _stop_at_blank_week(df)
-    data = _checkForTempValue(df,Y11)
+    data11 = _checkForTempValue(df,Y11)
+    data12 = _checkForTempValue(df,Y12)
 
     def nonempty_block(frame: pd.DataFrame, subcols: list[str]) -> pd.DataFrame:
         out = frame[FIXEDCOL + subcols].copy()
         mask_all_empty = out[subcols].apply(lambda s: s.fillna("").astype(str).str.strip()).eq("").all(axis=1)
         return out.loc[~mask_all_empty]
     
-    year11 = nonempty_block(data, Y11)
-    #year12 = nonempty_block(data, Y12)
+    data11 = nonempty_block(data11, Y11)
+    data12 = nonempty_block(data12, Y12)
 
     out_dir = Path(outdir)
     out_dir.mkdir(parents=True, exist_ok=True)
-    year11.to_json(out_dir / "year11.json", orient="records", indent=2)
-    #year12.to_json(out_dir / "year12.json", orient="records", indent=2 )
+    data11.to_json(out_dir / "year11.json", orient="records", indent=2)
+    data12.to_json(out_dir / "year12.json", orient="records", indent=2 )
 
     print("Json saved to:", out_dir.resolve())
 
